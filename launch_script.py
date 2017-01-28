39d5be1e6e149ecaff6e5a2987bf3257a7a310f5
James Lu <jamtlu@gmail.com>
2017-01-28 17:05:30 -0500

    Update launch script

--- launch_script.py
+++ launch_script.py
@@ -1,11 +1,16 @@
-import os
+import os, time, requests, uuid
 
 print(
+
 os.system("boinccmd --project_attach http://universeathome.pl/universe/ 26444_75b49274e2105b916557e65ea236e7e4")
+
 )
 
-import time
+instance_id = str(uuid.UUID())
+lead_url = "a001-bonic.herokuapp.com"
+report_back_postfix = "/internals/v0.01-ping/"
 
 while True:
-   time.sleep(1000)
+   time.sleep(10)
+   requests.post(lead_url + report_back_postfix + instance_id)
    pass
