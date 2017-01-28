7224c2fd2269ecb78f956f4d5338f2c077e1d75f
James Lu <jamtlu@gmail.com>
2017-01-28 17:50:12 -0500

    Change stuff

--- launch_script.py
+++ launch_script.py
@@ -2,12 +2,12 @@ import os, time, requests, uuid
 
 print(os.system("mkdir boinc-data"))  # create boinc data directory
 
-print(os.system("boinc &"))
+print(os.system("boinc --dir ./boinc-data & "))
 
 time.sleep(0.5)  # wait for boinc to start
 
 print(
-os.system("boinccmd --host 127.0.0.1 --dir ./boinc-data project_attach http://universeathome.pl/universe/ 26444_75b49274e2105b916557e65ea236e7e4")
+os.system("boinccmd --host 127.0.0.1 --dir ./boinc-data --passwd  26444_75b49274e2105b916557e65ea236e7e4 project_attach http://universeathome.pl/universe/ &")
 )
 
 instance_id = str(uuid.uuid4())
