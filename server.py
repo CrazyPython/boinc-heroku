4321c0d581104417fa43274132d4035f9307f3c6
James Lu <jamtlu@gmail.com>
2017-01-28 20:28:37 -0500

    Add server.py that redirects to Boincoku dashboard

---
+++ server.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# coding=utf-8
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import urllib
+
+PORT_NUMBER = 8000
+BONICOKU_URL = 'https://boincoku.herokuapp.com/'
+
+class Handler(BaseHTTPRequestHandler):
+    def do_HEAD(s):
+        s.send_response(301)
+        s.send_header("Location", BONICOKU_URL)
+        s.end_headers()
+
+
+
+try:
+    server = HTTPServer(('', PORT_NUMBER), Handler)
+    print('Started httpserver on port ' + str(PORT_NUMBER))
+    server.serve_forever()
+except KeyboardInterrupt:
+    server.socket.close()
